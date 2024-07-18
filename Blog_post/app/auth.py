928edec5851eb4eb1318cb62fd342b6dc198db3e
ZahraaNalbanPharmaciaty <zahraa.nalban@pharmaciaty.com>
2024-07-18 16:16:30 +0400

    Comments added

--- Blog_post/app/auth.py
+++ Blog_post/app/auth.py
@@ -34,12 +34,20 @@ def verify_password(user_password, password):
 
 #Check if User Exists
 def get_user(db, username: str):
+    '''
+        Check if user exists in database and returns user password
+    '''
     if username in db:
         user_data=db[username]
         return schemas.UserInDB(**user_data)
     
 #User Authentication
 def authenticate_user(db, username: str, password: str):
+    '''
+        Validates user authentication,
+        if user does not exist or if there is a password mismatch return false
+        if credentials meet uuser details are returned
+    '''
     user = get_user(db, username)
     if not user:
         return False
@@ -49,25 +57,63 @@ def authenticate_user(db, username: str, password: str):
 
 #Creating Authoriation Token 
 def create_access_token(data:dict, expires_delta: timedelta or None= None):
+    '''
+        Generated authentication token on login for user authentication
+
+        Args:
+            data: data to encode token
+            expires_delta: sets expiration time to token
+        
+        Returns:
+            token generated and returned        
+    '''
+
+    #Generates copy of data
     to_encode = data.copy()
+
+    #sets expirations time
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
         expire = datetime.utcnow() + timedelta(minutes=15)
+
+    #updates expiration time
     to_encode.update({"exp":expire})
+
+    #generate token
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 #Check current user
 async def get_current_user(token: str = Depends(oauth2_scheme)):
+    '''
+        Decodes the token and gets user details.
+        if user details are not found unauthorized error will be displayed
+
+        Args:
+            token: jwt token
+
+        Returns:
+            user details after authenticating with token
+    '''
     try:
+        # Decodes token with secret key and algorithm
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        # Gets username from token payload
         username: str = payload.get("sub")
+
+        #if username not found, 401 Unauthorized exception is raised
         if username is None:
             raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
+        
+        #create token_data object with username
         token_data = schemas.TokenData(username=username)
+
     except JWTError:
+        #if error is generated while decoding data, 401 Unauthorized exception is raised
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
+    
     user = get_user(user_db, username=token_data.username)
     if user is None:
         raise HTTPException(status_code = status.HTTP_401_UNAUTHORIZED, detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
@@ -75,6 +121,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
 #Get current user details
 async def get_current_active_user(current_user: schemas.UserInDB = Depends(get_current_user)):
+    '''
+        Get current user details and check if the user is active
+    '''
     if current_user.disabled:
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
